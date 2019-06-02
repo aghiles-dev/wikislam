597fe97c2f2786dea4da69be85a924c3b8ad05c1
octo-agka <abkaoudjt@octo.com>
2019-06-02 15:18:27 +0200

    display surates

--- src/ui/main-content/surates-list/AppSuratesList.vue
+++ src/ui/main-content/surates-list/AppSuratesList.vue
@@ -3,7 +3,20 @@
     <progress v-if="state.isLoading" class="progress is-small is-primary" max="100"/>
 
     <div v-else>
-      {{state.surates.length}}
+      <div v-for="surate in state.surates"
+           :key="surate.id"
+           class="card">
+        <span>{{surate.id}}</span><br>
+        <span>{{surate.beginsWithBismillah}}</span><br>
+        <span>{{surate.surateNumber}}</span><br>
+        <span>{{surate.arabicName}}</span><br>
+        <span>{{surate.frenchName}}</span><br>
+        <span>{{surate.name}}</span><br>
+        <span>{{surate.numberOfVerses}}</span><br>
+        <span>{{surate.revelationOrder}}</span><br>
+        <span>{{surate.revelationPlace}}</span><br>
+
+      </div>
     </div>
   </div>
 </template>
